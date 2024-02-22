2e98f6b86a9bb9546bdfcf9d1f6dad13f505f8cc
Diego Randa <diegorandazzo69@gmail.com>
2024-02-22 08:42:46 +0100

    Aggiungere i file di progetto.

---
+++ AS2324.Randazzo.Diego.3G.es13lab/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AS2324.Randazzo.Diego._3G.es13lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("indicare il numero di persone");
+            int dim= Convert.ToInt32(Console.ReadLine());
+            int[] età = new int[dim];
+            int[] peso = new int[dim];
+
+        }
+    }
+}
